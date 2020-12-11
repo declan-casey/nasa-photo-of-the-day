96f259d0904e7c2d44f4184c598a62cd6c359702
declan-casey <dpcaseyboy99@gmail.com>
2020-12-10 20:11:47 -0500

    done

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import axios from "axios"
 import "./App.css";
 import Header from "./Components/Header"
 import Body from "./Components/Body"
+import styled from 'styled-components'
 
 
-const API_KEY = "BunglmqsToUEiuibkGyeDiz98BBc3ugzRxAje8ft";
+const API_KEY = "5r57XZvxgGc8XZ9brMblKashYzujTQsHut2ZS3eM";
 const URL = "https://api.nasa.gov/planetary"
+
+const StyledApp = styled.div `
+background-image : url("https://images.unsplash.com/flagged/photo-1564292284203-6a4ddcc5bda3?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTI4fHxzcGFjZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60");
+background-size: cover;
+background-repeat: no-repeat;
+`
 
 function App() {
 
   const [photo, setPhoto] = useState([]);
+
 
   useEffect(() => {
     const fetchAPOD = () => {
@@ -24,11 +32,13 @@ function App() {
     }
     fetchAPOD()
   }, [])
+
   return (
-    <div className="App">
+    <StyledApp className="App">
       <Header title = {photo.title} date = {photo.date}/>
-      <Body photo = {photo.url} explanation = {photo.explanation}/>
-    </div>
+      <Body photo = {photo.url} explanation = {photo.explanation} />
+    </StyledApp>
+
   );
 }
 
